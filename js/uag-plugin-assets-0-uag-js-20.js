372e7897e8dee8ad4b9d816a44d856b3cdd70dcf
marwa-nano <marwa.nanodegree@gmail.com>
2024-05-01 10:10:40 +0200

    first commit

---
+++ js/uag-plugin-assets-0-uag-js-20.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"c1a45bc1","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"marwa.arabcoders@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-c1a45bc1', 20 );
+});
+window.addEventListener( 'DOMContentLoaded', function() {
+	UAGBLottie._run( {"block_id":"d1108848","lottieSource":"url","lottieURl":"https:\/\/lottie.host\/d22353af-afe7-469c-9ff9-5b133c06e631\/Vk0os0cNya.json","jsonLottie":"","height":"","heightTablet":"","heightMob":"","width":"","widthTablet":"","widthMob":"","backgroundColor":"","backgroundHColor":"","loop":true,"speed":1,"reverse":false,"playOn":"none"}, 'uagb-block-d1108848' );
+});
+ });
